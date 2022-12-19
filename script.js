d732938aa67805966e4f0a8b5e701670a9046d44
vatsal <vatsalsinghkv@gmail.com>
2022-12-19 10:13:34 +0530

    v1.1

--- script.js
+++ script.js
@@ -1,80 +1,77 @@
 // DATA
-let PCOLOR = "#00eeff";
-let SCOLOR = "#007399";
-let getHour = document.getElementById("hr");
-let getMin = document.getElementById("min");
-let getSec = document.getElementById("sec");
+const PCOLOR = '#00eeff';
+const SCOLOR = '#007399';
+const hoursEl = document.getElementById('hr');
+const minutesEl = document.getElementById('min');
+const secondsEl = document.getElementById('sec');
 
-let getAmPm = document.querySelectorAll(".am-pm span");
-let getDays = document.querySelectorAll(".days li");
+const amPmEl = document.querySelectorAll('.am-pm span');
+const daysEl = document.querySelectorAll('.days li');
 
-let getDate = document.getElementById("date");
-let getMonth = document.getElementById("month");
-let getYear = document.getElementById("year");
+const dateEl = document.getElementById('date');
+const monthEl = document.getElementById('month');
+const yearEl = document.getElementById('year');
 
 // CALLING
 // IIFE
-let intially = (function() {
-    reset();
-}());
+(function () {
+  reset();
+})();
 
 setInterval(time, 1000);
 
-
 // FUNCTION
 function reset() {
-    getDays.forEach(day => day.style.color = SCOLOR);
-    getAmPm.forEach(amPm => amPm.style.color = SCOLOR);
+  daysEl.forEach((day) => (day.style.color = SCOLOR));
+  amPmEl.forEach((amPm) => (amPm.style.color = SCOLOR));
 
-    let date = new Date();
-    setDayAmPm(date.getDay(), date.getHours() < 12);
-    setTime(date.getHours(), date.getMinutes(), date.getSeconds());
-    setDate(date.getDate(), date.getMonth() + 1, date.getFullYear());
+  const date = new Date();
+
+  setDayAmPm(date.getDay(), date.getHours() < 12);
+  setTime(date.getHours(), date.getMinutes(), date.getSeconds());
+  setDate();
 }
 
 function time() {
-    let date = new Date();
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec =  date.getSeconds();
-    let am = (hour < 12);
-    let day = date.getDay();
+  const date = new Date();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  const am = hour < 12;
+  const day = date.getDay();
 
-    if(hour == 0) {
-        reset();
-    }
+  if (hour == 0) {
+    reset();
+  }
 
-    setTime(hour, min, sec);
+  setTime(hour, min, sec);
+  setDayAmPm(day, am);
 }
 
 function setDayAmPm(day, am) {
-    getDays[day].style.color = PCOLOR;
-    if(am) {
-        getAmPm[0].style.color = PCOLOR;
-    } else {
-        getAmPm[1].style.color = PCOLOR;
-    }
-} 
-
-function setTime(hour, min, sec) {
-    hour = hour > 12 ? hour - 12 : hour;
-    hour = hour == 0 ? 12 : hour;
-    hour = hour < 10 ? "0" + hour : hour;
-    
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
-
-    getHour.innerHTML = hour;
-    getMin.innerHTML = min;
-    getSec.innerHTML = sec;
+  daysEl[day].style.color = PCOLOR;
+  if (am) {
+    amPmEl[0].style.color = PCOLOR;
+  } else {
+    amPmEl[1].style.color = PCOLOR;
+  }
 }
 
-function setDate(date, month, year) {
-    date = date > 12 ? date - 12 : date;
-    date = date < 10 ? "0" + date : date;
-    month = month < 10 ? "0" + month : month;
+function setTime(hour, min, sec) {
+  hour = hour > 12 ? hour - 12 : hour;
+  hour = hour == 0 ? 12 : hour;
+  hour = hour < 10 ? '0' + hour : hour;
 
-    getDate.innerHTML = date;
-    getMonth.innerHTML = month;
-    getYear.innerHTML = year;
+  min = min < 10 ? '0' + min : min;
+  sec = sec < 10 ? '0' + sec : sec;
+
+  hoursEl.innerHTML = hour;
+  minutesEl.innerHTML = min;
+  secondsEl.innerHTML = sec;
+}
+
+function setDate() {
+  dateEl.innerHTML = new Date().toLocaleDateString('en-IN', {
+    dateStyle: 'medium',
+  });
 }
